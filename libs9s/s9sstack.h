a7949f81a6fe0c00805cfe9d30df284560c2bd7e
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2016-08-25 10:22:54 +0200

    Working on JSON parser.

---
+++ libs9s/s9sstack.h
@@ -0,0 +1,55 @@
+/* 
+ * Copyright (C) 2011-2015 severalnines.com
+ */
+#pragma once
+
+#include <vector>
+
+template <typename T>
+class S9sStack : public std::vector<T>
+{
+    public:
+        void push(const T &item);
+        T pop();
+        T &top();
+        const T &top() const;
+};
+
+template <typename T>
+T
+S9sStack<T>::pop()
+{
+    assert(!this->empty());
+
+    T retval = this->front();
+    this->erase(this->begin());
+    return retval;
+}
+
+template <typename T>
+T &
+S9sStack<T>::top()
+{
+    assert(!this->empty());
+
+    return this->front();
+}
+
+template <typename T>
+const T &
+S9sStack<T>::top() const
+{
+    assert(!this->empty());
+
+    return this->front();
+}
+
+template <typename T>
+void 
+S9sStack<T>::push(
+        const T &item)
+{
+    this->insert(this->begin(), item);
+}
+
+
