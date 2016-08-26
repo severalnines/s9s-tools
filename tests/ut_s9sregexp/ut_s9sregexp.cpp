41804133bad80d33c4adeb55ce41a7d2d12e47db
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2016-08-26 15:43:40 +0200

    regexps: new unit test

--- tests/ut_s9sregexp/ut_s9sregexp.cpp
+++ tests/ut_s9sregexp/ut_s9sregexp.cpp
@@ -17,33 +17,31 @@
  * You should have received a copy of the GNU General Public License
  * along with Foobar. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "ut_s9soptions.h"
+#include "ut_s9sregexp.h"
 
-#include "S9sOptions"
 #include <cstdio>
 #include <cstring>
 
-
+#include "S9sRegExp"
 
 //#define DEBUG
 #include "s9sdebug.h"
 
-UtS9sOptions::UtS9sOptions()
+UtS9sRegExp::UtS9sRegExp()
 {
     S9S_DEBUG("");
 }
 
-UtS9sOptions::~UtS9sOptions()
+UtS9sRegExp::~UtS9sRegExp()
 {
 }
 
 bool
-UtS9sOptions::runTest(const char *testName)
+UtS9sRegExp::runTest(const char *testName)
 {
     bool retval = true;
 
     PERFORM_TEST(testCreate,        retval);
-    PERFORM_TEST(testReadOptions01, retval);
 
     return retval;
 }
@@ -52,34 +50,11 @@ UtS9sOptions::runTest(const char *testName)
  * Testing the constructors.
  */
 bool
-UtS9sOptions::testCreate()
+UtS9sRegExp::testCreate()
 {
-    S9sOptions *options = S9sOptions::instance();
-
-    S9S_VERIFY(options != NULL);
-    S9sOptions::uninit();
-    S9S_VERIFY(S9sOptions::sm_instance == NULL);
-
     return true;
 }
 
-bool
-UtS9sOptions::testReadOptions01()
-{
-    S9sOptions *options = S9sOptions::instance();
-    const char *argv[]  = { "/bin/binary_name", "node", "--verbose", NULL };
-    int   argc   = 3;
-    bool  success;
-
-    success = options->readOptions(&argc, (char**)argv);
-    S9S_VERIFY(success);
-    S9S_COMPARE(options->m_myName, "binary_name");
-    S9S_COMPARE(options->m_operationMode, S9sOptions::Node);
-
-    S9sOptions::uninit();
-    return true;
-}
-
-S9S_UNIT_TEST_MAIN(UtS9sOptions)
+S9S_UNIT_TEST_MAIN(UtS9sRegExp)
 
 
