1c88940f86ccfd670a63a4351f580614c5e3c29d
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2016-09-15 12:22:52 +0200

    New class to handle nodes.

---
+++ libs9s/s9snode.h
@@ -0,0 +1,37 @@
+/*
+ * Severalnines Tools
+ * Copyright (C) 2016  Severalnines AB
+ *
+ * This file is part of s9s-tools.
+ *
+ * s9s-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "S9sVariantMap"
+
+/**
+ * A class that represents a node/host/server. 
+ */
+class S9sNode
+{
+    public:
+        S9sNode();
+        S9sNode(const S9sVariantMap &properties);
+
+        virtual ~S9sNode();
+
+    private:
+        S9sVariantMap    m_properties;
+};
