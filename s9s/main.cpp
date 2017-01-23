d8d11797263090b7276fb6ed7b3e02dd011583f3
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-01-23 08:08:11 +0100

    Fixing copyright message.

--- s9s/main.cpp
+++ s9s/main.cpp
@@ -9,13 +9,13 @@
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * S9sTools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar. If not, see <http://www.gnu.org/licenses/>.
+ * along with S9sTools. If not, see <http://www.gnu.org/licenses/>.
  */
 #include "library.h"
 #include "S9sOptions"
