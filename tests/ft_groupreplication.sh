eec48a461d0b8a892f01ba2363fc69e3ac95f01e
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-02-22 14:59:51 +0100

    New test file.

---
+++ tests/ft_groupreplication.sh
@@ -0,0 +1,421 @@
+#! /bin/bash
+MYNAME=$(basename $0)
+MYBASENAME=$(basename $0 .sh)
+MYDIR=$(dirname $0)
+STDOUT_FILE=ft_errors_stdout
+VERBOSE=""
+LOG_OPTION="--wait"
+CLUSTER_NAME="${MYBASENAME}_$$"
+CLUSTER_ID=""
+ALL_CREATED_IPS=""
+PIP_CONTAINER_CREATE=$(which "pip-container-create")
+
+# This is the name of the server that will hold the linux containers.
+CONTAINER_SERVER="core1"
+
+# The IP of the node we added last. Empty if we did not.
+LAST_ADDED_NODE=""
+
+cd $MYDIR
+source include.sh
+
+#
+# Prints usage information and exits.
+#
+function printHelpAndExit()
+{
+cat << EOF
+Usage: $MYNAME [OPTION]... [TESTNAME]
+ Test script for s9s to check various error conditions.
+
+ -h, --help       Print this help and exit.
+ --verbose        Print more messages.
+ --log            Print the logs while waiting for the job to be ended.
+ --server=SERVER  The name of the server that will hold the containers. 
+
+EOF
+    exit 1
+}
+
+
+ARGS=$(\
+    getopt -o h \
+        -l "help,verbose,log,server:" \
+        -- "$@")
+
+if [ $? -ne 0 ]; then
+    exit 6
+fi
+
+eval set -- "$ARGS"
+while true; do
+    case "$1" in
+        -h|--help)
+            shift
+            printHelpAndExit
+            ;;
+
+        --verbose)
+            shift
+            VERBOSE="true"
+            ;;
+
+        --log)
+            shift
+            LOG_OPTION="--log"
+            ;;
+
+        --server)
+            shift
+            CONTAINER_SERVER="$1"
+            shift
+            ;;
+
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+if [ -z "$S9S" ]; then
+    echo "The s9s program is not installed."
+    exit 7
+fi
+
+CLUSTER_ID=$($S9S cluster --list --long --batch | awk '{print $1}')
+
+#if [ ! -d data -a -d tests/data ]; then
+#    echo "Entering directory tests..."
+#    cd tests
+#fi
+if [ -z "$PIP_CONTAINER_CREATE" ]; then
+    printError "The 'pip-container-create' program is not found."
+    printError "Don't know how to create nodes, giving up."
+    exit 1
+fi
+
+#
+# Creates and starts a new 
+#
+function create_node()
+{
+    local ip
+
+    ip=$(pip-container-create --server=$CONTAINER_SERVER)
+    echo $ip
+}
+
+#
+# $1: the name of the cluster
+#
+function find_cluster_id()
+{
+    local name="$1"
+    local retval
+    local nTry=0
+
+    while true; do
+        retval=$($S9S cluster --list --long --batch --cluster-name="$name")
+        retval=$(echo "$retval" | awk '{print $1}')
+
+        if [ -z "$retval" ]; then
+            printVerbose "Cluster '$name' was not found."
+            let nTry+=1
+
+            if [ "$nTry" -gt 10 ]; then
+                echo 0
+                break
+            else
+                sleep 3
+            fi
+        else
+            printVerbose "Cluster '$name' was found with ID ${retval}."
+            echo "$retval"
+            break
+        fi
+    done
+}
+
+function grant_user()
+{
+    $S9S user --create --cmon-user=$USER --generate-key \
+        >/dev/null 2>/dev/null
+}
+
+#
+#
+#
+function testPing()
+{
+    pip-say "Pinging controller."
+
+    #
+    # Pinging. 
+    #
+    $S9S cluster --ping 
+
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "Exit code is not 0 while pinging controller."
+        pip-say "The controller is off line. Further testing is not possible."
+    else
+        pip-say "The controller is on line."
+    fi
+}
+
+#
+# This test will allocate a few nodes and install a new cluster.
+#
+function testCreateCluster()
+{
+    local nodes
+    local nodeName
+    local exitCode
+
+    pip-say "The test to create MySQL replication cluster is starting now."
+    nodeName=$(create_node)
+    nodes+="$nodeName;"
+    FIRST_ADDED_NODE=$nodeName
+    ALL_CREATED_IPS+=" $nodeName"
+    
+    nodeName=$(create_node)
+    nodes+="$nodeName;"
+    ALL_CREATED_IPS+=" $nodeName"
+    
+    nodeName=$(create_node)
+    nodes+="$nodeName"
+    ALL_CREATED_IPS+=" $nodeName"
+    
+    #
+    # Creating a MySQL replication cluster.
+    #
+    $S9S cluster \
+        --create \
+        --cluster-type=group_replication \
+        --nodes="$nodes" \
+        --vendor=oracle \
+        --cluster-name="$CLUSTER_NAME" \
+        --provider-version=5.7 \
+        $LOG_OPTION
+
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "Exit code is not 0 while creating cluster."
+        return 1
+    fi
+
+    CLUSTER_ID=$(find_cluster_id $CLUSTER_NAME)
+    printVerbose "CLUSTER_ID: '$CLUSTER_ID'"
+
+    if [ "$CLUSTER_ID" -gt 0 ]; then
+        printVerbose "Cluster ID is $CLUSTER_ID"
+    else
+        failure "Cluster ID '$CLUSTER_ID' is invalid"
+    fi
+}
+
+#
+# 
+#
+function testConfig()
+{
+    local exitCode
+    
+    pip-say "The test to check configuration is starting now."
+
+    #
+    # Listing the configuration values.
+    #
+    cat <<EOF
+    $S9S node 
+        --list-config 
+        --nodes=$FIRST_ADDED_NODE:3306
+EOF
+
+    $S9S node \
+        --list-config \
+        --nodes=$FIRST_ADDED_NODE:3306 
+
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# This test will add one new node to the cluster.
+#
+function testAddNode()
+{
+    local nodeName
+    local nodes
+    local exitCode
+
+    pip-say "The test to add node is starting now."
+    printVerbose "Creating node..."
+    nodeName=$(create_node)
+    LAST_ADDED_NODE="$nodeName"
+    ALL_CREATED_IPS+=" $nodeName"
+    nodes+="$nodeName"
+    
+    printVerbose "Created node '$LAST_ADDED_NODE'."
+    printVerbose "*** nodes: '$nodes'"
+
+    #
+    # Adding a node to the cluster.
+    #
+    printVerbose "Adding node:"
+    $S9S cluster \
+        --add-node \
+        --cluster-id=$CLUSTER_ID \
+        --nodes="$nodes" \
+        $LOG_OPTION
+    
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# This test will remove the last added node.
+#
+function testRemoveNode()
+{
+    if [ -z "$LAST_ADDED_NODE" ]; then
+        printVerbose "Skipping test."
+    fi
+    
+    pip-say "The test to remove node is starting now."
+    
+    #
+    # Removing the last added node.
+    #
+    $S9S cluster \
+        --remove-node \
+        --cluster-id=$CLUSTER_ID \
+        --nodes="$LAST_ADDED_NODE" \
+        $LOG_OPTION
+    
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# This will perform a rolling restart on the cluster
+#
+function testRollingRestart()
+{
+    local exitCode
+    
+    pip-say "The test of rolling restart is starting now."
+
+    #
+    # Calling for a rolling restart.
+    #
+    $S9S cluster \
+        --rolling-restart \
+        --cluster-id=$CLUSTER_ID \
+        $LOG_OPTION
+    
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# Stopping the cluster.
+#
+function testStop()
+{
+    local exitCode
+
+    pip-say "The test to stop cluster is starting now."
+
+    #
+    # Stopping the cluster.
+    #
+    $S9S cluster \
+        --stop \
+        --cluster-id=$CLUSTER_ID \
+        $LOG_OPTION
+    
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# Dropping the cluster from the controller.
+#
+function testDrop()
+{
+    local exitCode
+
+    pip-say "The test to drop the cluster is starting now."
+
+    #
+    # Starting the cluster.
+    #
+    $S9S cluster \
+        --drop \
+        --cluster-id=$CLUSTER_ID \
+        $LOG_OPTION
+    
+    exitCode=$?
+    printVerbose "exitCode = $exitCode"
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode}"
+    fi
+}
+
+#
+# This will destroy the containers we created.
+#
+function testDestroyNodes()
+{
+    pip-say "The test is now destroying the nodes."
+    pip-container-destroy \
+        --server=$CONTAINER_SERVER \
+        $ALL_CREATED_IPS \
+        >/dev/null 2>/dev/null
+}
+
+#
+# Running the requested tests.
+#
+startTests
+grant_user
+
+if [ "$1" ]; then
+    for testName in $*; do
+        runFunctionalTest "$testName"
+    done
+else
+    runFunctionalTest testPing
+    runFunctionalTest testCreateCluster
+    runFunctionalTest testConfig
+
+    #runFunctionalTest testAddNode
+    #runFunctionalTest testRemoveNode
+    #runFunctionalTest testRollingRestart
+    #runFunctionalTest testStop
+    runFunctionalTest testDrop
+    runFunctionalTest testDestroyNodes
+fi
+
+endTests
+
+
