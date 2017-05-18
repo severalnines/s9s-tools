dde68ff7942fbbc0b76101056c6c66f77bbca701
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-05-18 12:29:09 +0200

    Implementing graphs.

---
+++ tests/ut_s9sgraph/ut_s9sgraph.cpp
@@ -0,0 +1,56 @@
+/*
+ * Severalnines Tools
+ * Copyright (C) 2016  Severalnines AB
+ *
+ * This file is part of s9s-tools.
+ *
+ * s9s-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s9s-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s9s-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ut_s9sgraph.h"
+
+#include "S9sGraph"
+
+//#define DEBUG
+#include "s9sdebug.h"
+
+UtS9sGraph::UtS9sGraph()
+{
+}
+
+UtS9sGraph::~UtS9sGraph()
+{
+}
+
+bool
+UtS9sGraph::runTest(const char *testName)
+{
+    bool retval = true;
+
+    PERFORM_TEST(testCreate01,      retval);
+
+    return retval;
+}
+
+/**
+ * Testing the constructor that parse the string representation.
+ */
+bool
+UtS9sGraph::testCreate01()
+{
+    S9sGraph graph1();
+
+    return true;
+}
+
+S9S_UNIT_TEST_MAIN(UtS9sGraph)
