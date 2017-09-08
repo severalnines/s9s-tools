02e564ce5538147f4f5c6dbf19d90bc03c8ecf9b
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-09-08 12:36:19 +0200

    S9sContainer: new class

---
+++ libs9s/s9scontainer.cpp
@@ -0,0 +1,188 @@
+/*
+ * Severalnines Tools
+ * Copyright (C) 2016  Severalnines AB
+ *
+ * This file is part of s9s-tools.
+ *
+ * s9s-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s9s-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s9s-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "s9scontainer.h"
+
+#include <S9sUrl>
+#include <S9sVariantMap>
+#include <S9sRpcReply>
+
+//#define DEBUG
+//#define WARNING
+#include "s9sdebug.h"
+
+S9sContainer::S9sContainer()
+{
+    m_properties["class_name"] = "CmonContainer";
+}
+ 
+S9sContainer::S9sContainer(
+        const S9sVariantMap &properties) :
+    m_properties(properties)
+{
+    if (!m_properties.contains("class_name"))
+        m_properties["class_name"] = "CmonContainer";
+}
+
+/**
+ * \param stringRep The string representation of the host, either a JSon string
+ *   or an url (e.g. "192.168.1.100:3306".
+ */
+S9sContainer::S9sContainer(
+        const S9sString &stringRep)
+{
+    bool success;
+
+    S9S_WARNING("stringRep : %s", STR(stringRep));
+    // Parsing as a JSon string, that's more specific.
+    success = m_properties.parse(STR(stringRep));
+    if (success)
+    {
+        S9S_WARNING("parsed as json");
+        m_url = m_properties["hostname"].toString();
+
+        if (m_properties.contains("port"))
+            m_url.setPort(m_properties["port"].toInt());
+
+        m_url.setProperties(m_properties);
+    } else {
+        S9S_WARNING("parsing as url");
+        // If not ok then parsing as an URL.
+        m_url = S9sUrl(stringRep);
+
+        m_properties = m_url.properties();
+        m_properties["hostname"] = m_url.hostName();
+
+        if (m_url.hasPort())
+            m_properties["port"] = m_url.port();
+    }
+   
+    if (m_url.hasProtocol())
+    {
+        S9sString protocol = m_url.protocol().toLower();
+
+        if (m_url.protocol() == "lxc")
+            m_properties["class_name"] = "CmonContainerServer";
+    }
+
+    if (!m_properties.contains("class_name"))
+        m_properties["class_name"] = "CmonHost";
+}
+
+S9sContainer::~S9sContainer()
+{
+}
+
+S9sContainer &
+S9sContainer::operator=(
+        const S9sVariantMap &rhs)
+{
+    setProperties(rhs);
+    
+    return *this;
+}
+
+/**
+ * \returns The S9sContainer converted to a variant map.
+ *
+ * \code{.js}
+ * {
+ *     "alias": "mqtt",
+ *     "class_name": "CmonContainer",
+ *     "hostname": "192.168.1.5",
+ *     "ip": "192.168.1.5",
+ *     "ipv4_addresses": [ "192.168.1.5" ],
+ *     "owner_group_id": 4,
+ *     "owner_group_name": "testgroup",
+ *     "owner_user_id": 3,
+ *     "owner_user_name": "pipas",
+ *     "parent_server": "core1",
+ *     "status": "RUNNING",
+ *     "type": "lxc"
+ * }, 
+ * \endcode
+ */
+const S9sVariantMap &
+S9sContainer::toVariantMap() const
+{
+    return m_properties;
+}
+
+/**
+ * \returns True if a property with the given key exists.
+ */
+bool
+S9sContainer::hasProperty(
+        const S9sString &key) const
+{
+    return m_properties.contains(key);
+}
+
+/**
+ * \returns The value of the property with the given name or the empty
+ *   S9sVariant object if the property is not set.
+ */
+S9sVariant
+S9sContainer::property(
+        const S9sString &name) const
+{
+    if (m_properties.contains(name))
+        return m_properties.at(name);
+
+    return S9sVariant();
+}
+
+/**
+ * \param name The name of the property to set.
+ * \param value The value of the property as a string.
+ *
+ * This function will investigate the value represented as a string. If it looks
+ * like a boolean value (e.g. "true") then it will be converted to a boolean
+ * value, if it looks like an integer (e.g. 42) it will be converted to an
+ * integer. Then the property will be set accordingly.
+ */
+void
+S9sContainer::setProperty(
+        const S9sString &name,
+        const S9sString &value)
+{
+    if (value.looksBoolean())
+    {
+        m_properties[name] = value.toBoolean();
+    } else if (value.looksInteger())
+    {
+        m_properties[name] = value.toInt();
+    } else {
+        m_properties[name] = value;
+    }
+}
+
+/**
+ * \param properties The properties to be set as a name -> value mapping.
+ *
+ * Sets all the properties in one step. All the existing properties will be
+ * deleted, then the new properties set.
+ */
+void
+S9sContainer::setProperties(
+        const S9sVariantMap &properties)
+{
+    m_properties = properties;
+}
+
---
+++ libs9s/s9scontainer.h
@@ -0,0 +1,50 @@
+/*
+ * Severalnines Tools
+ * Copyright (C) 2016  Severalnines AB
+ *
+ * This file is part of s9s-tools.
+ *
+ * s9s-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s9s-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s9s-tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "S9sVariantMap"
+#include "S9sUrl"
+#include "S9sCluster"
+
+/**
+ * A class that represents a node/host/server. 
+ */
+class S9sContainer
+{
+    public:
+        S9sContainer();
+        S9sContainer(const S9sVariantMap &properties);
+        S9sContainer(const S9sString &stringRep);
+
+        virtual ~S9sContainer();
+
+        S9sContainer &operator=(const S9sVariantMap &rhs);
+
+        bool hasProperty(const S9sString &key) const;
+        S9sVariant property(const S9sString &name) const;
+        void setProperty(const S9sString &name, const S9sString &value);
+
+        const S9sVariantMap &toVariantMap() const;
+        void setProperties(const S9sVariantMap &properties);
+
+    private:
+        S9sVariantMap    m_properties;
+        S9sUrl           m_url;
+};
