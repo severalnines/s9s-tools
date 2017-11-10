74c2687d9bd221810ec91fff964293d43b229a4e
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-11-10 17:45:17 +0100

    Polishing.

--- pipscripts/pip-virtualbox-sample.sh
+++ pipscripts/pip-virtualbox-sample.sh
@@ -7,5 +7,26 @@ VERBOSE=""
 LOGFILE=""
 SERVER=""
 
-VBoxManage list runningvms --long
+function get_vm_os()
+{
+    local uuid="$1"
 
+    VBoxManage showvminfo $uuid | grep "^Guest OS:" | awk -F: '{print $2}'
+}
+
+#VBoxManage list runningvms --long
+UUIDS=$(\
+    VBoxManage list runningvms --long | \
+    grep "UUID:            " | \
+    awk 'BEGIN{FS="UUID:            "}{print $2}')
+
+for uuid in $UUIDS; do
+    vm=$(VBoxManage showvminfo $uuid | sed -e'/^USB Device Filters:/,$ d' | grep "Name:"  | awk 'BEGIN{FS="Name:            "}{print $2}')
+    os=$(get_vm_os $uuid)
+
+    echo "    uuid : '$uuid'"
+    echo "      vm : '$vm'"
+    echo "      os : '$os'"
+    echo " command : VBoxManage showvminfo $uuid"
+    VBoxManage controlvm "$vm" screenshotpng ${vm}.png
+done
