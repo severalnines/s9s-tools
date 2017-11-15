e5fda63afd00d046ed8f9628ffd9fea0c3a67b96
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-11-15 10:31:47 +0100

    Polishing test scripts.

--- tests/ft_errors.sh
+++ tests/ft_errors.sh
@@ -167,7 +167,7 @@ function testJobOperations()
         return 1
     fi
     
-    expected="The --list, --log and --wait options are mutually exclusive."
+    expected="The main options are mutually exclusive."
     output=$($S9S job --list --log --job-id=5 2>&1)
     if [ "$output" != "$expected" ]; then
         failure "Error message not as expected when operation is missing"
