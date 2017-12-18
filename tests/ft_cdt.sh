6aa88e754056bda85b18d00edc8df42494399e50
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2017-12-18 08:54:02 +0100

    Added test.

---
+++ tests/ft_cdt.sh
@@ -0,0 +1,214 @@
+#! /bin/bash
+MYNAME=$(basename $0)
+MYBASENAME=$(basename $0 .sh)
+MYDIR=$(dirname $0)
+STDOUT_FILE=ft_errors_stdout
+VERBOSE=""
+VERSION="0.0.3"
+LOG_OPTION="--wait"
+CLUSTER_NAME="${MYBASENAME}_$$"
+CLUSTER_ID=""
+PIP_CONTAINER_CREATE=$(which "pip-container-create")
+CONTAINER_SERVER=""
+
+cd $MYDIR
+source include.sh
+
+#
+# Prints usage information and exits.
+#
+function printHelpAndExit()
+{
+cat << EOF
+Usage: $MYNAME [OPTION]... [TESTNAME]
+ Test script for s9s to check various error conditions.
+
+ -h, --help       Print this help and exit.
+ --verbose        Print more messages.
+ --print-json     Print the JSON messages sent and received.
+ --log            Print the logs while waiting for the job to be ended.
+ --print-commands Do not print unit test info, print the executed commands.
+ --reset-config   Remove and re-generate the ~/.s9s directory.
+ --server=SERVER  Use the given server to create containers.
+
+EOF
+    exit 1
+}
+
+ARGS=$(\
+    getopt -o h \
+        -l "help,verbose,print-json,log,print-commands,reset-config,server:" \
+        -- "$@")
+
+if [ $? -ne 0 ]; then
+    exit 6
+fi
+
+eval set -- "$ARGS"
+while true; do
+    case "$1" in
+        -h|--help)
+            shift
+            printHelpAndExit
+            ;;
+
+        --verbose)
+            shift
+            VERBOSE="true"
+            OPTION_VERBOSE="--verbose"
+            ;;
+
+        --log)
+            shift
+            LOG_OPTION="--log"
+            ;;
+
+        --print-json)
+            shift
+            OPTION_PRINT_JSON="--print-json"
+            ;;
+
+        --print-commands)
+            shift
+            DONT_PRINT_TEST_MESSAGES="true"
+            PRINT_COMMANDS="true"
+            ;;
+
+        --reset-config)
+            shift
+            OPTION_RESET_CONFIG="true"
+            ;;
+
+        --server)
+            shift
+            CONTAINER_SERVER="$1"
+            shift
+            ;;
+
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+if [ -z "$S9S" ]; then
+    printError "The s9s program is not installed."
+    exit 7
+fi
+
+#
+# This function is used to cut one cell (of a given row and column) from a table
+# formatted text.
+#
+function index_table()
+{
+    local text="$1"
+    local row=$2
+    local column=$3
+    local line
+    local counter=1
+
+    line=$(echo "$text" | head -n $row | tail -n 1)
+    for cell in $line; do
+        if [ $counter -eq "$column" ]; then
+            echo "$cell"
+            break
+        fi
+
+        let counter+=1
+    done
+}
+
+function testCreateUser()
+{
+    local exitCode
+
+    print_title "Creating a Superuser"
+
+    mys9s user \
+        --create \
+        --cmon-user="system" \
+        --password="secret" \
+        --group="admins" \
+        --email-address="laszlo@severalnines.com" \
+        --first-name="Laszlo" \
+        --last-name="Pere"   \
+        --generate-key \
+        --new-password="pipas" \
+        "pipas"
+
+    exitCode=$?
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode} while creating user through RPC"
+        exit 1
+    fi
+
+    # An extra key for the SSH login to the container.
+    mys9s user \
+        --add-key \
+        --public-key-file="/home/$USER/.ssh/id_rsa.pub" \
+        --public-key-name="The SSH key"
+    
+    exitCode=$?
+    if [ "$exitCode" -ne 0 ]; then
+        failure "The exit code is ${exitCode} while registering key"
+        exit 1
+    fi
+}
+
+function testRootFolder()
+{
+    local lines
+    local row
+    local column
+    local cell
+    local required
+
+    print_title "Testing CDT"
+
+    mys9s tree --tree
+    mys9s tree --list
+
+    lines=$(s9s tree --list --batch)
+
+    #
+    # 
+    #
+    row=1
+    column=5
+    cell=$(index_table "$lines" $row $column)
+    required="/"
+    if [ "$cell" != "$required" ]; then
+        failure "The cell at $row,$column should be '$required', it is '$cell'"
+        return 1
+    fi
+
+    column=4
+    cell=$(index_table "$lines" $row $column)
+    required="admins"
+    if [ "$cell" != "$required" ]; then
+        failure "The cell at $row,$column should be '$required', it is '$cell'"
+        return 1
+    fi
+
+    return 0
+}
+
+#
+# Running the requested tests.
+#
+startTests
+reset_config
+
+if [ "$1" ]; then
+    for testName in $*; do
+        runFunctionalTest "$testName"
+    done
+else
+    runFunctionalTest testCreateUser
+    runFunctionalTest testRootFolder
+fi
+
+endTests
+
