c5dcac1692d234094d4c28eccd0c5b33611ab3ed
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2019-06-11 09:23:59 +0200

    Polishing test script.

--- tests/ft_cluster_lxc.sh
+++ tests/ft_cluster_lxc.sh
@@ -281,7 +281,7 @@ function createCluster()
         --containers="$container_name1;$container_name2" \
         --os-user=sisko \
         --os-key-file="$config_dir/sisko.key" \
-        $LOG_OPTION 
+        --log
 
     check_exit_code $?
     check_container_ids --galera-nodes
