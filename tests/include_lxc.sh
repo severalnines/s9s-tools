cb355a6c8d3912d4778c9ac2bb1d9d0fb5f787cc
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2019-10-10 12:16:09 +0200

    Added new test for c2c.

---
+++ tests/include_lxc.sh
@@ -0,0 +1,27 @@
+#
+# This will register the container server. 
+#
+function registerServer()
+{
+    local class
+
+    print_title "Registering Container Server"
+
+    #
+    # Creating a container.
+    #
+    mys9s server \
+        --register \
+        --servers="lxc://$CONTAINER_SERVER" 
+
+    check_exit_code_no_job $?
+
+    #
+    # Checking the state and the class name... 
+    #
+    check_container_server \
+        --class        CmonLxcServer \
+        --server-name  "$CONTAINER_SERVER" \
+        --cloud        "lxc"
+}
+
