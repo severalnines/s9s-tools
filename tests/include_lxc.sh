82a8fd909ac8db4c665bd9e2beae85d7572bf01b
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2020-05-20 09:27:04 +0200

    Polishing test scripts for jenkins.

--- tests/include_lxc.sh
+++ tests/include_lxc.sh
@@ -6,6 +6,12 @@ function registerServer()
     local class
 
     print_title "Registering Container Server"
+    cat <<EOF | paragraph
+  This is the registerServer test from the include_lxc.sh file that will
+  register an LXC server and check if it is indeed registered. We do this
+  in tests where the controller itself needs to create conatiners.
+EOF
+
     begin_verbatim
 
     #
@@ -16,6 +22,8 @@ function registerServer()
         --servers="lxc://$CONTAINER_SERVER" 
 
     check_exit_code_no_job $?
+    mys9s tree --cat /$CONTAINER_SERVER/.runtime/state
+
     end_verbatim    
 
     #
@@ -27,7 +35,6 @@ function registerServer()
         --cloud        "lxc" \
         --class       "CmonLxcServer"
 
-    mys9s tree --cat /$CONTAINER_SERVER/.runtime/state
 
 }
 
