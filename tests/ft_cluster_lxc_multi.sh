57ce07e053e9f8e281857216f6b60d0e312870dd
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2020-05-26 11:45:40 +0200

    Fixing test scripts.

--- tests/ft_cluster_lxc_multi.sh
+++ tests/ft_cluster_lxc_multi.sh
@@ -12,7 +12,8 @@ CMON_CLOUD_CONTAINER_SERVER=""
 CLUSTER_NAME="${MYBASENAME}_$$"
 
 cd $MYDIR
-source include.sh
+source ./include.sh
+source ./include_user.sh
 
 #
 # Prints usage information and exits.
@@ -35,7 +36,7 @@ Usage:
  --server=SERVER  Use the given server to create containers.
 
 SUPPORTED TESTS:
-  o createUser       Creates a user to work with.
+  o createUserSisko  Creates a user to work with.
   o registerServer   Registers a new container server. No software installed.
   o createCluster1   Creates a cluster.
 
@@ -118,50 +119,6 @@ if [ -z "$CONTAINER_SERVER" ]; then
     exit 6
 fi
 
-function createUser()
-{
-    local config_dir="$HOME/.s9s"
-    local myself
-
-    #
-    # Adding an extra user that we will use for testing.
-    #
-    print_title "Creating a New User"
-
-    mys9s user \
-        --create \
-        --cmon-user=system \
-        --password=secret \
-        --title="Captain" \
-        --first-name="Benjamin" \
-        --last-name="Sisko"   \
-        --email-address="sisko@ds9.com" \
-        --generate-key \
-        --group=ds9 \
-        --create-group \
-        --batch \
-        "sisko"
-    
-    check_exit_code_no_job $?
-
-    ls -lha $HOME/.s9s/sisko*
-
-    if [ ! -f "$config_dir/sisko.key" ]; then
-        failure "Secret key file 'sisko.key' was not found."
-        exit 0
-    fi
-
-    if [ ! -f "$config_dir/sisko.pub" ]; then
-        failure "Public key file 'sisko.pub' was not found."
-        exit 0
-    fi
-
-    myself=$(s9s user --whoami)
-    if [ "$myself" != "$USER" ]; then
-        failure "Whoami returns $myself instead of $USER."
-    fi
-}
-
 #
 # This will register the container server. 
 #
@@ -170,6 +127,7 @@ function registerServer()
     local class
 
     print_title "Registering Container Server"
+    begin_verbatim
 
     #
     # Creating a container.
@@ -192,13 +150,13 @@ function registerServer()
 
     if [ "$class" != "CmonLxcServer" ]; then
         failure "Created server has a '$class' class and not 'CmonLxcServer'."
-        exit 1
     fi
     
     #
     # Checking the state... TBD
     #
     mys9s tree --cat /$CONTAINER_SERVER/.runtime/state
+    end_verbatim
 }
 
 function createCluster1()
@@ -211,6 +169,7 @@ function createCluster1()
     # Creating a Cluster.
     #
     print_title "Creating a Cluster on LXC"
+    begin_verbatim
 
     mys9s cluster \
         --create \
@@ -222,6 +181,7 @@ function createCluster1()
         --nodes="$container1;$container2;$container3" \
         --containers="$container1;$container2;$container3" \
         $LOG_OPTION
+    end_verbatim
 }
 
 function createCluster2()
@@ -233,6 +193,7 @@ function createCluster2()
     # Creating a Cluster.
     #
     print_title "Creating a Cluster on LXC"
+    begin_verbatim
 
     mys9s cluster \
         --create \
@@ -244,6 +205,7 @@ function createCluster2()
         --nodes="$container1;$container2" \
         --containers="$container1;$container2" \
         $LOG_OPTION
+    end_verbatim
 }
 
 function createCluster3()
@@ -255,6 +217,7 @@ function createCluster3()
     # Creating a Cluster.
     #
     print_title "Creating a Cluster on LXC"
+    begin_verbatim
 
     mys9s cluster \
         --create \
@@ -266,6 +229,7 @@ function createCluster3()
         --nodes="$container1;$container2" \
         --containers="$container1;$container2" \
         $LOG_OPTION
+    end_verbatim
 }
 
 #
@@ -279,7 +243,7 @@ grant_user
 #EVENT_HANDLER_PID=$!
 
 if [ "$OPTION_INSTALL" ]; then
-    runFunctionalTest createUser
+    runFunctionalTest createUserSisko
     runFunctionalTest registerServer
     runFunctionalTest createCluster1
     runFunctionalTest createCluster2
@@ -288,7 +252,7 @@ elif [ "$1" ]; then
         runFunctionalTest "$testName"
     done
 else
-    runFunctionalTest createUser
+    runFunctionalTest createUserSisko
     runFunctionalTest registerServer
     runFunctionalTest createCluster1
     runFunctionalTest createCluster2
