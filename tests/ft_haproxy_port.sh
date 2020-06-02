5e4610bff87aa21e6527f0da03612d126ba0a9a0
Laszlo Pere <pipas@gamma.ttk.pte.hu>
2020-06-02 11:16:19 +0200

    Haproxy: ro/rw ports test.

--- tests/ft_haproxy_port.sh
+++ tests/ft_haproxy_port.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./ft_galera.sh --provider-version=5.7 --vendor="percona" --rw-port=4406 --ro-port=4407 $*
+./ft_haproxy.sh --provider-version=5.7 --vendor="percona" --rw-port=4406 --ro-port=4407 $*
