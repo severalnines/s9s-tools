50779050871b2ae296d15ff06eedc6b1ba56d969
Laszlo Pere <laszlo@severalnines.com>
2020-12-22 09:14:14 +0100

    LDAP tests update.

--- tests/include_ldap.sh
+++ tests/include_ldap.sh
@@ -56,48 +56,6 @@ EOF
     end_verbatim
 }
 
-function testLdapUser3()
-{
-    local username="lpere"
-
-    print_title "Checking LDAP Groups"
-    cat <<EOF | paragraph
-  Logging in with a user that is part of an LDAP group and also not in the root
-  of the LDAP tree. Checking that the ldapgroup is there and the user is in the
-  ldap related group.
-EOF
-
-    begin_verbatim
-    mys9s user \
-        --list \
-        --long \
-        --cmon-user="$username" \
-        --password=p
-
-    check_exit_code_no_job $?
-   
-    mys9s user \
-        --stat \
-        --long \
-        --cmon-user="$username" \
-        --password=p \
-        lpere
-
-    check_exit_code_no_job $?
-
-    check_user \
-        --user-name    "lpere"  \
-        --full-name    "Laszlo Pere" \
-        --email        "pipas@domain.hu" \
-        --cdt-path     "/" \
-        --group        "ldapgroup" \
-        --dn           "cn=lpere,cn=ldapgroup,dc=homelab,dc=local" \
-        --origin       "LDAP"
-
-    end_verbatim
-}
-
-
 # 
 # Distinguished name logins, these are not supported any more and so these tests
 # are probably deprecated.
