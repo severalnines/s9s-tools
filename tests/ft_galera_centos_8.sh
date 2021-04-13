99a9401816acbe4cce5672fbadc4f0e6c35450b3
Laszlo Pere <laszlo@severalnines.com>
2021-04-13 11:08:40 +0200

    Trying to fix a failure.

--- tests/ft_galera_centos_8.sh
+++ tests/ft_galera_centos_8.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./ft_galera.sh --os-vendor=centos --os-release=8 --provider-version=5.7 --vendor="percona" $*
+./ft_galera.sh --log --os-vendor=centos --os-release=8 --provider-version=5.7 --vendor="percona" $*
