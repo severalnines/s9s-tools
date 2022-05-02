0e5ae2d90d6abd99c2fdebc77820ef7cdccb7d0a
David Kedves <kedz@kedz.eu>
2022-05-02 13:48:53 +0200

    version bumper script initial

---
+++ version-bump.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# David Kedves <kedazo@gmail.com>
+
+if [ ! -d debian ]; then
+    echo "Please execute this script inside the project directory."
+    exit 1
+fi
+
+# remove old source packages
+# rm -fv ../s9s-tools*
+
+export LC_ALL=C
+export LC_DATE=C
+DEBEMAIL="support@severalnines.com"
+DEBFULLNAME="Severalnines"
+export EMAIL=${DEBEMAIL}
+export DEBFULLNAME=${DEBFULLNAME}
+VERSION="1.9.`date +%Y%m%d%H`"
+# RPM needs the date/time in a very specific format
+RPMDATE=`date +"%a %b %_d %Y"`
+
+# debian changelog update
+debchange --newversion "${VERSION}-release1" "Release ${VERSION}."
+# rpm project version update
+sed "s/%changelog/%changelog\n* ${RPMDATE} ${DEBFULLNAME} <${DEBEMAIL}> ${VERSION}\n- Release ${VERSION}./" -i project.spec
+
+#dpkg-buildpackage -rfakeroot -S --no-sign
+
