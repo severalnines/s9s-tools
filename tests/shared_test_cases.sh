11668ece0f4b7da672e781530f75fee629c65742
csjpeter <csjpeter@gmail.com>
2022-08-24 08:01:30 +0200

    Revert "Experiemting: what if shared_test_cases.sh::createController creates container by ubuntu and not ubuntu-s9s template"
    This reverts commit 0613724e61a645dc414d4c21faedab407efaf2f6.

--- tests/shared_test_cases.sh
+++ tests/shared_test_cases.sh
@@ -40,8 +40,7 @@ compile the controller source. This may take some time.
 EOF
 
     begin_verbatim
-    #node_ip=$(create_node --autodestroy --template "ubuntu-s9s" "$node_name") 
-    node_ip=$(create_node --autodestroy --template "ubuntu" "$node_name") 
+    node_ip=$(create_node --autodestroy --template "ubuntu-s9s" "$node_name") 
     SECONDARY_CONTROLLER_IP="$node_ip"
     SECONDARY_CONTROLLER_URL="https://$SECONDARY_CONTROLLER_IP:9556"
     end_verbatim
