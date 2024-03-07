61152c187b70619c402b889d7292f14b7a706f8e
alvaro-vinuela <alvaro@severalnines.com>
2024-03-07 12:51:10 +0100

    CLUS-3586 redis sharded cluster deployment and optional fields

--- libs9s/s9srpcclient.h
+++ libs9s/s9srpcclient.h
@@ -551,6 +551,12 @@ class S9sRpcClient
                 const S9sString      &osUserName,
                 const S9sString      &psqlVersion);
 
+        bool createRedisSharded(
+                const S9sVariantList &hosts,
+                const S9sString      &osUserName,
+                const S9sString      &redisVersion);
+
+
         bool createRedisSentinel(
                 const S9sVariantList &hosts,
                 const S9sString      &osUserName,
@@ -561,11 +567,16 @@ class S9sRpcClient
                 const S9sString      &osUserName,
                 const S9sString      &elasticVersion);
 
-
         bool registerRedisCluster(
                 const S9sVariantList &hosts,
                 const S9sString      &osUserName,
                 const S9sString      &redisVersion);
+
+        bool registerRedisSharded(
+                const S9sVariantList &hosts,
+                const S9sString      &osUserName,
+                const S9sString      &redisVersion);
+
 
         bool createMsSqlSingle(
                 const S9sVariantList &hosts,
