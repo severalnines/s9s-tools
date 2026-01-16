7f1502acaea698fa558f6a301f69092a937b0848
Császár Péter <peter@severalnines.com>
2026-01-16 20:53:35 +0100

    CLUS-6905 HaProxy: For existing clusters it is better to pass normal hostname, and not internal.

--- libs9s/s9srpcclient.cpp
+++ libs9s/s9srpcclient.cpp
@@ -5663,7 +5663,7 @@ S9sRpcClient::addHaProxy(
         node = otherNodes[idx].toNode();
         port = node.hasPort() ? node.port() : 3306;
 
-        tmp.sprintf("%s:%d:%s", STR(node.internalHostName()), port, "active");
+        tmp.sprintf("%s:%d:%s", STR(node.hostName()), port, "active");
 
         if (!nodeAddresses.empty())
             nodeAddresses += ";";
